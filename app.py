a90596c33446ac64cca33ab132db8624547c7a84
Mona Lisa <alexanderyoung@Alexanders-MacBook-Pro.local>
2019-11-02 17:52:39 -0400

    Added the 'api/v1.0/<start> functionality

--- app.py
+++ app.py
@@ -127,6 +127,51 @@ def temperature():
 
     return jsonify(tobs_dict_list)
 
+
+@app.route("/api/v1.0/<start>")
+def weather_short(start):
+
+    session = Session(engine)
+
+    # This function called `calc_temps` will accept start date and end date in the format '%Y-%m-%d' 
+    # and return the minimum, average, and maximum temperatures for that range of dates
+    def calc_temps(start_date, end_date):
+        """TMIN, TAVG, and TMAX for a list of dates.
+    
+        Args:
+            start_date (string): A date string in the format %Y-%m-%d
+            end_date (string): A date string in the format %Y-%m-%d
+        
+        Returns:
+            TMIN, TAVE, and TMAX
+        """
+        minimum = func.min(Measurement.tobs)
+        average = func.avg(Measurement.tobs)
+        maximum = func.max(Measurement.tobs)
+        return session.query(minimum, average, maximum).\
+            filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+    results = calc_temps(start, '2017-08-23')
+    print(results)
+
+    
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    new_weather = []
+    for result in results:
+        weather_dict = {}
+        weather_dict["TMIN"] = result[0]
+        weather_dict["TAVG"] = result[1]
+        weather_dict["TMAX"] = result[2]
+        new_weather.append(weather_dict)
+
+    return jsonify(new_weather)
+
+
+
 @app.route("/api/v1.0/<start>/<end>")
 def weather_long(start, end):
 
@@ -167,7 +212,7 @@ def weather_long(start, end):
         weather_dict["TMAX"] = result[2]
         all_weather.append(weather_dict)
 
-    return f"""<header>{jsonify(all_weather)} </header><body bgcolor="mediumseagreen"> </br> </body>""", 
+    return jsonify(all_weather)
 
 
 
